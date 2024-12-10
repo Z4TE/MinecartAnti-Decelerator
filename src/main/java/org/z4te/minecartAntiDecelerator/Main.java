9ad48f2ed428b0c1fa1893bd540366918f097fe7
Z4TE <kayakun426@gmail.com>
2024-12-10 09:53:33 +0900

    【悲報】ぶっ壊れた

--- src/main/java/org/z4te/minecartAntiDecelerator/Main.java
+++ src/main/java/org/z4te/minecartAntiDecelerator/Main.java
@@ -9,10 +9,15 @@ import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public final class Main extends JavaPlugin implements Listener {
 
-    private static final double DECELERATION_MODIFIER = 0.98;
-    private static final double MIN_ACCELERATION_THRESHOLD = 0.05;
+    private final HashMap<UUID, Vector> previousVelocities = new HashMap<>();
+
+    private static final double DECELERATION_MODIFIER = 0.99;
+    private static final double MIN_ACCELERATION_THRESHOLD = 0.02;
 
     @Override
     public void onEnable() {
@@ -30,19 +35,20 @@ public final class Main extends JavaPlugin implements Listener {
     public void onMinecartMove(VehicleMoveEvent event) {
         if (event.getVehicle() instanceof Minecart minecart){
 
+            UUID minecartId = minecart.getUniqueId();
+
             Vector currentVelocity = minecart.getVelocity();
-            Vector previousVelocity = new Vector(
-                    event.getFrom().getX() - event.getTo().getX(),
-                    event.getFrom().getY() - event.getTo().getY(),
-                    event.getFrom().getZ() - event.getTo().getZ()
-            );
+            Vector previousVelocity = previousVelocities.getOrDefault(minecartId, new Vector(0, 0, 0));
+
 
             double acceleration = currentVelocity.distance(previousVelocity);
+            Bukkit.broadcastMessage(String.valueOf(acceleration));
 
-            if (acceleration < MIN_ACCELERATION_THRESHOLD) {
+            if (acceleration > MIN_ACCELERATION_THRESHOLD) {
                 currentVelocity.multiply(DECELERATION_MODIFIER);
                 minecart.setVelocity(currentVelocity);
             }
+
         }
     }
 }
